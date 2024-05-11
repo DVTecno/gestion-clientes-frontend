abf553c94e5f45d8cccf8b2bd4057e2c0ad544d1
Diego  Veras <verasdiegoca78@gmail.com>
2024-05-11 20:37:23 -0300

    Update API.URL.js

--- app-clientes/src/const/API.URL.js
+++ app-clientes/src/const/API.URL.js
@@ -1,4 +1,5 @@
 export const API_URL = {
-  URL: "https://gestionclientedvtecno.onrender.com/api/v1",
+ // URL: "https://gestionclientedvtecno.onrender.com/api/v1",
+  URL: "http://localhost:8080/api/v1",
   CLIENTS: "/clientes"
 };
